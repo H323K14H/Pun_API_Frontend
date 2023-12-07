3d5dcbc36318e951994aecd45966358dac3ee30c
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-12-07 10:01:59 +0000

    - Created form that will store fan details - Amended application CSS

---
+++ src/components/PunForm.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const PunForm = ({ onNewFanSubmission }) => {
+
+    // const navigate = useNavigate();
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [comment, setComment] = useState("");
+
+
+    const updateFanName = (event) => {
+        setName(event.target.value);
+    }
+    const updateFanEmail = (event) => {
+        setEmail(event.target.value);
+    }
+    const updateFanComment = (event) => {
+        setComment(event.target.value);
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        if (!name || !email || !comment) {
+            alert("We need your details, chief!🧐🧐");
+            return;
+        }
+
+        const newFan = {
+            name: name,
+            email: email,
+            comment: comment,
+            id: Date.now()
+        }
+
+        setName("");
+        setEmail("");
+        setComment("");
+        onNewFanSubmission(newFan);
+    }
+
+    return (
+        <section>
+            <h2>Post new comment:</h2>
+            <form onSubmit={(event) => handleFormSubmit(event)}>
+                <label htmlFor="name-input">Name:</label>
+                <input
+                    type="text"
+                    id="name-input"
+                    onInput={(event) => updateFanName(event)}
+                    value={name}
+                />
+                <label htmlFor="email-input">Email:</label>
+                <input
+                    type="email"
+                    id="email-input"
+                    onInput={(event) => updateFanEmail(event)}
+                    value={email}
+                />
+                <label htmlFor="comment-input">Comment:</label>
+                <input
+                    type="text"
+                    id="comment-input"
+                    onInput={(event) => updateFanComment(event)}
+                    value={comment}
+                />
+                <input type="submit" value="Add New Fan" />
+            </form>
+        </section>
+    )
+}
+
+export default PunForm;
--- src/containers/PunContainer.js
+++ src/containers/PunContainer.js
@@ -1,11 +1,28 @@
 import Pun from "../components/Pun";
 import { useEffect, useState } from "react";
 import PunButton from "../components/PunButton";
+import PunForm from "../components/PunForm";
 
 
 const PunContainer = () => {
 
     const [pun, setPun] = useState(null);
+    const [punFans, setPunFans] = useState(
+        [
+            {
+                name: "Robert",
+                email: "Rob.puns@aol.com",
+                comment: "I was on the edge.... But once I pressed that button, my life changed forever",
+                id: 0
+            },
+            {
+                name: "Ashley",
+                email: "Ash.404@yahoo.com",
+                comment: "The kids and I love generating a pun to get us started for the day!",
+                id: 0
+            }
+        ]
+    );
 
     // GET MAPPING
     const getRandomPun = async () => {
@@ -18,9 +35,15 @@ const PunContainer = () => {
         getRandomPun();
     }, [])
 
+    const handleNewFan = (newFan) => {
+        const updatedFans = [...punFans, newFan];
+        setPunFans(updatedFans);
+    }
+
     return (
         <body>
             <h1>Random Pun</h1>
+            <PunForm onNewFanSubmission={handleNewFan}/>
             {pun ? <Pun pun={pun} /> : <p>😩Life changing pun coming up😩...</p>}
             <br />
             <br />
