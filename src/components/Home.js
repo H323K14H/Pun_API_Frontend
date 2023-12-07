12f81fcc3e3e3ec3154107da1380ec47f2c97aec
Nathan Parmar <nathparmar@icloud.com>
2023-12-07 16:12:11 +0000

    added some CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,35 @@ const Home = () => {
   return (
     <>
       <nav>
-          <ul>
-              <Link to="/">Home</Link>
-              <Link to="/random-pun">Random Puns</Link> 
-              <Link to="/comment-section">Post comment:</Link> 
-          </ul>
+          
+              <div>
+                <Link className="links" to="/">Home</Link>
+              </div>
+              <div>
+                <Link className="links" to="/random-pun">Random Puns</Link> 
+              </div>
+              <div>
+                <Link className="links" to="/comment-section">Fan Page</Link> 
+              </div>
+          
       </nav>
+
+      <div className='links'>
+          <p>
+            Welcome to our pun application. Have some 🎸pun🎸 with us!!!!
+          </p>
+          <p>
+            If you don't like puns please direct to http://localhost:3000/get-in-the-bin
+          </p>
+          <p>
+            Brought to you by yours truly 
+          </p>
+          <p>
+            H323 & N4T
+          </p>
+
+      </div>
+
       <Outlet />
     </>
   )
